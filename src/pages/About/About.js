3fc3c3e6d7e78cee5fd846768947a0f4b3c1b5d0
Jahray0 <jaegle.maxime@gmail.com>
2022-11-30 15:32:24 +0100

    ajout card

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,11 @@
-import Title from '../../components/Title/Title';
-import styles from './About.module.scss'
-
+import Title from "../../components/Title/Title";
+import styles from "./About.module.scss";
 
 function About() {
   return (
     <div className={styles.main}>
-      <Title text = {'A propos'}/>
+      <Title text={"A propos"} />
+      <h1>404 Page not found</h1>
     </div>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import Title from "../../components/Title/Title";
 import styles from "./Home.module.scss";
 import img from "../../assets/img.png";
 import Banner from "../../components/Banner/Banner";
+import Card from "../../components/Card/Card";
 
 //import { useState } from "react";
 //import CardFruit from "../../components/CardFruit/CardFruit";
@@ -10,13 +11,7 @@ function Home() {
   return (
     <div className={styles.main}>
       <Banner />
-      <Title text={`Page d'accueil`} />
-
-      <ul>
-        <li>a</li>
-        <li>b</li>
-        <li>c</li>
-      </ul>
+      <Card />
     </div>
   );
 }
